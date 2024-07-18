912684a99b2e253e551426b60d6b3043d28c7e27
brijaldeveloper <brijalreactnative@gmail.com>
2024-07-18 15:46:01 +0530

    some change

--- Component/SingleSelection.js
+++ Component/SingleSelection.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
 
 const SingleSelection = ({ title, onPress }) => {
   return (
